477e44c5fe615d041e896a0cea9d7e5b932aa8d3
pleshakoff <pleshakoff@gmail.com>
2019-12-08 17:30:48 +0300

    Инициализация данных

--- src/main/resources/data-postgres.sql
+++ src/main/resources/data-postgres.sql
@@ -1,10 +1,10 @@
 INSERT INTO public.users (id, enabled, id_group, password, role, username)
-VALUES (1, true, 21, '$2a$10$kb27tY1oqDxSBzx9xEhRe.F70uX2cAm4qvphS1Gf0ppSNtOlho4EK', 'ROLE_ADMIN', 'admin@parcom.com');
+VALUES (1, true, 21, '$2a$10$kb27tY1oqDxSBzx9xEhRe.F70uX2cAm4qvphS1Gf0ppSNtOlho4EK', 'ROLE_ADMIN', 'admin@mail.com');
 
 INSERT INTO public.users (id, enabled, id_group, password, role, username)
-VALUES (2, true, 21, '$2a$10$kb27tY1oqDxSBzx9xEhRe.F70uX2cAm4qvphS1Gf0ppSNtOlho4EK', 'ROLE_MEMBER', 'mom@parcom.com');
+VALUES (2, true, 21, '$2a$10$kb27tY1oqDxSBzx9xEhRe.F70uX2cAm4qvphS1Gf0ppSNtOlho4EK', 'ROLE_MEMBER', 'molly@weasley.com');
 
 INSERT INTO public.users (id, enabled, id_group, password, role, username)
-VALUES (3, true, 21, '$2a$10$kb27tY1oqDxSBzx9xEhRe.F70uX2cAm4qvphS1Gf0ppSNtOlho4EK', 'ROLE_PARENT', 'dad@parcom.com');
+VALUES (3, true, 21, '$2a$10$kb27tY1oqDxSBzx9xEhRe.F70uX2cAm4qvphS1Gf0ppSNtOlho4EK', 'ROLE_PARENT', 'artur@weasley.com');
 
 SELECT setval('hibernate_sequence', 100, true);
