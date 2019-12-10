6c1d31bd34c2142975a9cc2e98c6f2e0dcaf326c
pleshakoff <pleshakoff@gmail.com>
2019-12-10 22:39:41 +0300

    Хосты

--- src/main/resources/data-postgres.sql
+++ src/main/resources/data-postgres.sql
@@ -7,4 +7,4 @@ VALUES (2, true, 21, '$2a$10$kb27tY1oqDxSBzx9xEhRe.F70uX2cAm4qvphS1Gf0ppSNtOlho4
 INSERT INTO public.users (id, enabled, id_group, password, role, username)
 VALUES (3, true, 21, '$2a$10$kb27tY1oqDxSBzx9xEhRe.F70uX2cAm4qvphS1Gf0ppSNtOlho4EK', 'ROLE_PARENT', 'artur@weasley.com');
 
-SELECT setval('hibernate_sequence', 100, true);
+-- SELECT setval('hibernate_sequence', 100, true);
