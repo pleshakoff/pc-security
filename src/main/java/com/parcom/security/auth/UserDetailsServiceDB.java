9daa6296992b9559025c7bb17a555d5c675bbed2
pleshakoff <pleshakoff@gmail.com>
2019-12-25 16:49:29 +0300

    Поменял создание пользователя

--- src/main/java/com/parcom/security/auth/UserDetailsServiceDB.java
+++ src/main/java/com/parcom/security/auth/UserDetailsServiceDB.java
@@ -4,12 +4,14 @@ package com.parcom.security.auth;
 import com.parcom.security.model.user.UserRepository;
 import com.parcom.security.model.user.User;
 import com.parcom.security_client.UserDetailsPC;
+import com.parcom.security_client.UserUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -37,9 +39,14 @@ public class UserDetailsServiceDB implements UserDetailsService {
     }
 
     static UserDetails buildUserDetails(User user) {
-        Set<GrantedAuthority> authorities = new HashSet<>();
-        authorities.add(new SimpleGrantedAuthority(user.getRole()));
-        return new UserDetailsPC(user.getUsername(),user.getPassword(),user.getId(),authorities,user.isEnabled(),user.getIdGroup(),user.getIdStudent());
+
+        return new UserDetailsPC(user.getUsername(),
+                user.getPassword(),
+                user.getId(),
+                AuthorityUtils.createAuthorityList(user.getRole()),
+                user.isEnabled(),
+                user.getIdGroup(),
+                user.getIdStudent());
     }
 
 
