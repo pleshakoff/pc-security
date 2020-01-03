698e788147e682551d34b7e143c934875778be8f
pleshakoff <pleshakoff@gmail.com>
2020-01-04 00:01:53 +0300

    Начал писать тесты

---
+++ src/test/java/com/parcom/security/model/user/UserServiceImplTest.java
@@ -0,0 +1,20 @@
+package com.parcom.security.model.user;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceImplTest {
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void setContext() {
+    }
+}
